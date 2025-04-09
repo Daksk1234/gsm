351f9e5b02e4f3f1affbaad39dcd2575ca60820a
Daksk1234 <95217969+Daksk1234@users.noreply.github.com>
2025-04-09 17:03:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ mongoose
 
 app.use('/api/calculation', calculationRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
